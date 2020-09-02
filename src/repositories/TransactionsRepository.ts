f7a78a8d870a5f16e2f169db8d1d74b78f9a7819
Joaquim Alves <jafn92@gmail.com>
2020-09-01 21:34:41 -0300

    end challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -15,14 +15,41 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    this.transactions.map(transaction => {
+      if(transaction.type == 'income'){
+        return income += transaction.value;
+      }
+
+      if(transaction.type == 'outcome') {
+        return outcome += transaction.value;
+      }
+    });
+
+    total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total
+    }
   }
 
-  public create(): Transaction {
+  public create(title: string, value: number, type: 'income' | 'outcome'): Transaction {
     // TODO
+    const transactions = new Transaction({title, value, type});
+
+    this.transactions.push(transactions);
+
+    return transactions;
   }
 }
 
